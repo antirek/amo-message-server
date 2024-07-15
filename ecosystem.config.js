5d841dc24efa7b3c92f3eda71180444b4c41197a
sergey <110@mobilon.ru>
2024-07-15 11:56:39 +0700

    fix

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps : [{
+    name: "admin-web",
+    script: "npm run start:admin-web",
+  }, {
+     name: 'incoming-messages-api',
+     script: 'npm run start:incoming-messages-api'
+  }]
+}
