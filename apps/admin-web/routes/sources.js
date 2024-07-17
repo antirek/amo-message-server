9c5a0d88a8a6a3b7bc70afce3e70797707e423fb
sergey <110@mobilon.ru>
2024-07-17 12:06:41 +0700

    fix

--- apps/admin-web/routes/sources.js
+++ apps/admin-web/routes/sources.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
   const response = await amoApiClient.getSources();
   console.log('response', response);
   // const sources = [{name: 'source1'}];
-  const sources = response._embedded.sources;
+  const sources = response?._embedded?.sources || [];
   res.render('sources', {sources});
 });
 
